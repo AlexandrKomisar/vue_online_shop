af9dcc08ed1850bfa123d94cd567e4a2e1d67920
AlexandrKomisar <monolitalex@gmail.com>
2023-02-01 07:40:45 +0200

    catalog

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="v-main-wrapper">
+        <p>{{ title }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'v-main-wrapper',
+    components: {},
+    props: {},
+    data() {
+        return {
+            title:'main wrapper'
+        }
+    },
+    computed: {},
+    methods: {},
+    watch: {},
+    mounted() {
+        console.log('HELLO')
+    }
+}
+</script>
+
+<style>
+.v-main-wrapper{
+    display: flex ;
+    justify-content: center;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+}
+</style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-main-wrapper/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import vMainWrapper from '@/components/v-main-wrapper.vue';
 
 export default {
+  components: { vMainWrapper },
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
