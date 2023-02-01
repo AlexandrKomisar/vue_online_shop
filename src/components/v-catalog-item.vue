f4e7f9f595a90a6bc22e92ec8dc6fe24d9923c66
AlexandrKomisar <monolitalex@gmail.com>
2023-02-01 22:10:18 +0200

    add product_data

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1></h1>
+    </div>
+</template>
+
+<script>
+import { objectExpression } from '@babel/types';
+
+export default {
+    name: 'v-catalog-item',
+    props: {
+        product_data: {
+            type: Object,
+            default() {
+                return {}
+            }
+            }
+        }
+    }
+</script>
---
+++ src/components/v-catalog.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>Каталог</h1>
+        <div class="v-catalog__list">
+            <v-catalog-item 
+            v-for="product in products" 
+            :key="product.article"
+            :product_data="product"
+            />
+    
+        </div>
+    </div>
+</template>
+
+<script>
+import vCatalogItem from './v-catalog-item.vue';
+
+export default {
+    name: 'v-catalog',
+    components: {
+        vCatalogItem
+    },
+    props: {},
+    data() {
+        return {
+            products: [
+                {
+                    image: '1.jpg',
+                    name: 'tovar 1',
+                    price: '100',
+                    article: 't1',
+                    available: true
+                },
+                {
+                    image: '2.jpg',
+                    name: 'tovar 2',
+                    price: '200',
+                    article: 't2',
+                    available: true
+                },
+            ]
+        }
+    }
+}
+</script>
